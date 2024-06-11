589729c0c826a3e4e718e98a2edaaf684d1df91c
araaki12345 <araaki12345@users.noreply.github.com>
2024-06-11 19:49:28 +0900

    feat:shadcn-uiの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import '@shadcn/ui/styles.css';
 import App from './App.tsx'
 import './index.css'
 
